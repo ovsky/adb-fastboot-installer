ddfaf9500565fcf539579a580b9d72ee099da689
ovsky <taycedd@gmail.com>
2025-01-13 17:13:36 +0100

    Update runner.cmd

--- runner.cmd
+++ runner.cmd
@@ -1,5 +1,5 @@
 @echo off
-TITLE Automatic ADB Installer v2.8.5
+TITLE Automatic ADB Installer v2.9.5
 COLOR 06
 
 call :isAdmin
@@ -8,7 +8,7 @@ if %errorlevel% == 0 (
     goto :run
 ) else (
     echo Requesting administrative privileges...
-    PING localhost -n 2 >NUL
+    PING localhost -n 1 >NUL
 
     goto :UACPrompt
 )
@@ -22,22 +22,22 @@ exit /b
 :run
 COLOR 03
 
-TITLE Automatic ADB Installer v2.8.5
+TITLE Automatic ADB Installer v2.9.5
 
     @echo off
-    echo Automatic ADB Installer v2.8.5
+    echo Automatic ADB Installer v2.9.5
     ECHO(
     echo Installing latest ADB and Fastboot...
     ECHO(
 
     echo -------------------
     echo Yay! We have been granted administrative rights!
-    PING localhost -n 2 >NUL
+    PING localhost -n 1 >NUL
     echo -------------------
-    PING localhofst -n 2 >NUL
+    PING localhofst -n 1 >NUL
 
     echo Copying ADB files...
-    PING localhost -n 2 >NUL
+    PING localhost -n 1 >NUL
 
     echo -------------------
     PING localhost -n 2 >NUL
@@ -51,39 +51,55 @@ TITLE Automatic ADB Installer v2.8.5
     echo         ADB folder with file(s) [15] copied.
     echo         ADBO folder with file(s) [6] copied.
     echo         ADB files initialized!
-    PING localhost -n 2 >NUL
     echo -------------------
+    PING localhost -n 1 >NUL
     echo LOG:    %USERPROFILE%\adb-installer.log
     echo -------------------
-    PING localhost -n 2 >NUL
+    PING localhost -n 1 >NUL
 
     IF [%1]==[] (
-        echo No parameters detected. Asking user...
+        echo No parameters detected. Asking user for input...
         GOTO QUESTION
     )
     
     if %1 == --silent (
         ECHO Detected --silent parameter. Silent installation with automatic driver detection...
-        PING localhost -n 2 >NUL
+        PING localhost -n 1 >NUL
         GOTO DRIVER
     )else if %1 == --silent-no-driver (
         ECHO Detected --silent-no-driver parameter. Skipping driver installation...
-        PING localhost -n 2 >NUL
+        PING localhost -n 1 >NUL
         GOTO SILENT-FINISH
     )
 
 
     :QUESTION
     color 02
-    @REM DEL PATH.TMP
-    ECHO(
-    SET /P ANSWER=Do you want to install device drivers? (Y/N)
-    IF /i {%ANSWER%}=={y} (GOTO DRIVER)
-    IF /i {%ANSWER%}=={yes} (GOTO DRIVER)
-    IF /i {%ANSWER%}=={n} (GOTO FINISH)
-    IF /i {%ANSWER%}=={no} (GOTO FINISH)
-    ECHO Bad answer! Use only Y/N or Yes/No
-    GOTO QUESTION
+
+    PING localhost -n 2 >NUL
+    echo -------------------
+
+    set userInput=
+    set /p userInput=Do you want to install ADB drivers? (Y/N) [TIMEOUT: 5s]: <nul
+    for /L %%i in (1,1,10) do (
+         choice /c YN /n /t 5 /d N >nul
+        if errorlevel 2 set userInput=N
+        if errorlevel 1 set userInput=Y
+        if defined userInput goto :userInputReceived
+    )
+    set userInput=X
+
+:userInputReceived
+    if /i "%userInput%"=="Y" (
+        echo You chose to install ADB drivers.
+        PING localhost -n 1 >NUL
+        GOTO DRIVER
+    ) else (
+        ECHO "%userInput%"
+        ECHO Skipping driver installation...
+        PING localhost -n 1 >NUL
+        GOTO FINISH
+    )
 
     :DRIVER
     ECHO -------------------
@@ -94,13 +110,15 @@ TITLE Automatic ADB Installer v2.8.5
     ECHO(
     ECHO Detected 32-bit OS. Installing 32-bit driver...
     ECHO Please continue driver installation if needed...
-    PING localhost -n 2 >NUL
+    PING localhost -n 1 >NUL
     IF [%1]==[] (
         START /wait %~dp0\driver\DPInst_x86 /f 2>>%USERPROFILE%\adb-installer.log
     )
-    IF %1 == --silent (
+    IF [%1] == --silent (
         START /wait %~dp0\driver\DPInst_x86 /q /f 2>>%USERPROFILE%\adb-installer.log
     )
+    PING localhost -n 1 >NUL
+
     GOTO FINISH
     )
 
@@ -108,20 +126,24 @@ TITLE Automatic ADB Installer v2.8.5
     ECHO(
     ECHO Detected 64-bit OS. Installing 64-bit driver...
     ECHO Please continue driver installation if needed...
-    PING localhost -n 2 >NUL
+    PING localhost -n 1 >NUL
+    
     IF [%1]==[] (
         START /wait %~dp0\driver\DPInst_x64 /f 2>>%USERPROFILE%\adb-installer.log
     )
-    IF %1 == --silent (
+    IF [%1] == --silent (
         START /wait %~dp0\driver\DPInst_x64 /q /f 2>>%USERPROFILE%\adb-installer.log
     )
+    PING localhost -n 1 >NUL
+
+
     GOTO FINISH
     )
 
     :FINISH
     color 03
     ECHO(
-    PING localhost -n 2 >NUL
+    PING localhost -n 1 >NUL
     echo -------------------
     @REM color 08
     PING localhost -n 1 >NUL
@@ -129,17 +151,26 @@ TITLE Automatic ADB Installer v2.8.5
     PING localhost -n 1 >NUL
     echo -------------------
 
-    pause
+    if [%1] equ [] (
+        if /i ["%userInput%"] neq [] (
+            pause
+        )
+    )
+
+    if [%1] neq [] (
+        PING localhost -n 4 >NUL
+    )
+    
     exit /b
     )
 
     :SILENT-FINISH
     @REM color 08
-    PING localhost -n 2 >NUL
+    PING localhost -n 1 >NUL
     echo -------------------
     echo Success! Now you can close this window and use the latest Android SDK ADB.
     echo -------------------
-    PING localhost -n 2 >NUL
+    PING localhost -n 1 >NUL
     exit /b
 
 
