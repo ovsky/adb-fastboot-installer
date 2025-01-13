53082905211691adb9af5682d7829e0b67b8c123
ovsky <taycedd@gmail.com>
2025-01-13 06:04:36 +0100

    Add new params, silent install functionality and update README

--- README.md
+++ README.md
@@ -1,21 +1,26 @@
-# 🤖 ADB & Fastboot Installer++ v2.5.0
+# 🤖 ADB & Fastboot Installer++ v2.8.5 [AFI++]
 
 ---
+
 ### 📒 Informations:
-Auto ADB Installer (ADB & Fastboot Installer) is the first ever self-updating debugging tool that automatically fetches latest libraries, drivers and execcutables - with every new API release, keeping it always up to date with latest Android, ADB & Fastboot features!
+
+Auto ADB Installer (ADB & Fastboot Installer) [AFI++] is the first ever self-updating debugging tool that automatically fetches latest libraries, drivers and executables - with every new API release, keeping it always up to date with latest Android, ADB & Fastboot features!
 
 Additionally, with this open-source, self-maintaining repository you always have a reliable ADB installer source, without having to search for links to hosts that are down or files that have expired, among tons of a malware and viruses.
 
 ### 📗 Idea:
+
 The idea behind the **AFI++** is:<br>
-*Simply the best, always up-to-date, totally reliable, open-source, and easy to use version of the system-wide `ADB and Fastboot`.*
+_Simply the best, always up-to-date, totally reliable, open-source, and easy to use version of the system-wide `ADB and Fastboot`._
 
 ### 📕 Updated API and Drivers:
-**Android ADB and Fastboot Installer++ v2.0.0**<br>
+
+**Android ADB and Fastboot Installer++ v2.8.5**<br>
 This is probaly the only installer with updated ADB libraries to the **latest Android SDK/NDK** and **Android API 35 / R15**.<br>
 You can start the SDK Update Action by using built-in GitHub Action - **`Update Android SDK`** and the repository will do the rest!
 
 ### ✨ Automated Workflow:
+
 Use the predefined **GitHub Actons**, to simply update the SDK, publish branch or build the project!<br>
 Work has never been so easy!
 
@@ -50,9 +55,14 @@ https://github.com/ovsky/adb-fastboot-installer/releases
 ###### Pre-build:
 
 1. Download and unpack latest release, then open the unpacked directory.
-2. Run `adb-install.exe` (as administrator).
+2. Run `adb-installer++.exe`.
 3. During the installation process, type [Y/N] [Yes/No] to specify your installation preferences.
 4. Everything is ready!
+
+###### Arguments:
+
+1. Use the `--silent` argument if you want to perform a fully automatic, silent installation.
+2. Use the `--silent-no-driver` if you want to perform a fully automatic, silent installation without the ADB driver.
 
 ###### Check:
 
@@ -80,7 +90,7 @@ When setting up Linux on your Android device, you have several options to choose
 | 🐥 Small Size | 📁 9MB                             |
 | 🐅 Super Fast | ⏳15 Secound Install               |
 | 🐉 All-in-One | 📦 ADB + Fastboot + Drivers        |
-| 🐈 Easy       | 📖 One-click-run              |
+| 🐈 Easy       | 📖 One-click-run                   |
 | 🪼 Open Source | ✅ Latest SDK, NDK and Drivers     |
 | 🐳 Clean      | 🔻 ADB and Drivers from Google SDK |
 
@@ -88,10 +98,10 @@ When setting up Linux on your Android device, you have several options to choose
 
 # [🐧 ️3. Author](#author)
 
-This repository is inspired and based on the original `15 seconds ADB Installer` script from famous `Snoop05`, who deserves many thanks!
+This repository is inspired on the original `15 seconds ADB Installer` script from famous `Snoop05`, who deserves many thanks!
 
-Author of this repo, actions, workflow and updates is `ovsky`, known profesionally as Przemysław Orłowski - owner of this GitHub Account and `orlowski.works` portfolio site (check it!).
-I have updated the installer files to the latest Android SDK/NDK and Android API 35 / R15. 
+Author of this repo, actions, workflow, installer and updates is `ovsky`, known profesionally as Przemysław Orłowski - owner of this GitHub Account and [`orlowski.works`](http://orlowski.works/) portfolio site (check it!).
+I have updated the installer files to the latest Android SDK/NDK and Android API 35 / R15.
 
 #
 
--- runner.cmd
+++ runner.cmd
@@ -1,5 +1,5 @@
 @echo off
-TITLE Automatic ADB Installer v2.8.0
+TITLE Automatic ADB Installer v2.8.5
 COLOR 06
 
 call :isAdmin
@@ -22,19 +22,19 @@ exit /b
 :run
 COLOR 03
 
-TITLE Automatic ADB Installer v2.8.0
+TITLE Automatic ADB Installer v2.8.5
 
     @echo off
-    echo Automatic ADB Installer v2.8.0
+    echo Automatic ADB Installer v2.8.5
     ECHO(
     echo Installing latest ADB and Fastboot...
     ECHO(
 
     echo -------------------
-    echo Yaay! We've got admin rights!
+    echo Yay! We have been granted administrative rights!
     PING localhost -n 2 >NUL
     echo -------------------
-    PING localhost -n 2 >NUL
+    PING localhofst -n 2 >NUL
 
     echo Copying ADB files...
     PING localhost -n 2 >NUL
@@ -46,13 +46,29 @@ TITLE Automatic ADB Installer v2.8.0
     copy %~dp0\adb.cmd %WINDIR%\System32\adb.cmd
     copy %~dp0\fastboot.cmd %WINDIR%\System32\fastboot.cmd
     XCOPY %~dp0\adb %WINDIR%\adb /y /q 1>nul 2>>%USERPROFILE%\adb-installer.log
-    @REM XCOPY %~dp0\adb %WINDIR%\adb /y /q 1>nul 2>>%USERPROFILE%\adb-installer.log 
-    echo         ADB files initialized.
+    echo         ADB folder with file(s) [15] copied.
+    echo         ADB files initialized!
     PING localhost -n 2 >NUL
     echo -------------------
     echo LOG:    %USERPROFILE%\adb-installer.log
     echo -------------------
     PING localhost -n 2 >NUL
+
+    IF [%1]==[] (
+        echo No parameters detected. Asking user...
+        GOTO QUESTION
+    )
+    
+    if %1 == --silent (
+        ECHO Detected --silent parameter. Silent installation with automatic driver detection...
+        PING localhost -n 2 >NUL
+        GOTO DRIVER
+    )else if %1 == --silent-no-driver (
+        ECHO Detected --silent-no-driver parameter. Skipping driver installation...
+        PING localhost -n 2 >NUL
+        GOTO SILENT-FINISH
+    )
+
 
     :QUESTION
     color 02
@@ -67,21 +83,37 @@ TITLE Automatic ADB Installer v2.8.0
     GOTO QUESTION
 
     :DRIVER
+    ECHO -------------------
     ECHO Installation of latest ADB driver...
     ECHO Please continue driver installation...
     IF DEFINED programfiles(x86) GOTO x64
     :x86
     ECHO(
-    ECHO Detected 32-bit OS. Installing 32-bit driver ...
+    ECHO Detected 32-bit OS. Installing 32-bit driver...
+    ECHO Please continue driver installation if needed...
     PING localhost -n 2 >NUL
-    START /wait %~dp0\driver\DPInst_x86 /f 2>>%USERPROFILE%\adb-installer.log
+    IF [%1]==[] (
+        START /wait %~dp0\driver\DPInst_x86 /f 2>>%USERPROFILE%\adb-installer.log
+    )
+    IF %1 == --silent (
+        START /wait %~dp0\driver\DPInst_x86 /q /f 2>>%USERPROFILE%\adb-installer.log
+    )
     GOTO FINISH
+    )
+
     :x64
     ECHO(
-    ECHO Detected 64-bit OS. Installing 64-bit driver ...
-    ECHO Please continue driver installation ...
+    ECHO Detected 64-bit OS. Installing 64-bit driver...
+    ECHO Please continue driver installation if needed...
     PING localhost -n 2 >NUL
-    START /wait %~dp0\driver\DPInst_x64 /f 2>>%USERPROFILE%\adb-installer.log
+    IF [%1]==[] (
+        START /wait %~dp0\driver\DPInst_x64 /f 2>>%USERPROFILE%\adb-installer.log
+    )
+    IF %1 == --silent (
+        START /wait %~dp0\driver\DPInst_x64 /q /f 2>>%USERPROFILE%\adb-installer.log
+    )
+    GOTO FINISH
+    )
 
     :FINISH
     color 03
@@ -93,12 +125,20 @@ TITLE Automatic ADB Installer v2.8.0
     echo Success! Now you can close this window and use the latest Android SDK ADB.
     PING localhost -n 1 >NUL
     echo -------------------
-    ECHO(
-
 
     pause
+    exit /b
+    )
 
-exit /b
+    :SILENT-FINISH
+    @REM color 08
+    PING localhost -n 2 >NUL
+    echo -------------------
+    echo Success! Now you can close this window and use the latest Android SDK ADB.
+    echo -------------------
+    PING localhost -n 2 >NUL
+    exit /b
+
 
 :UACPrompt
     echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
